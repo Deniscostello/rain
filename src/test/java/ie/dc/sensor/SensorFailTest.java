008b13fb7c333f99c6b0355cdd9da37e8969f13d
Denis Costello <G00396309@atu.ie>
2024-12-11 09:31:00 -0500

    Unit fail test on min and max length of sensor Id

--- src/test/java/ie/dc/sensor/SensorFailTest.java
+++ src/test/java/ie/dc/sensor/SensorFailTest.java
@@ -1,4 +1,27 @@
 package ie.dc.sensor;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SensorFailTest {
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void testSensorIdMinFailure() {
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> new Sensor("1","s1", "temperature", 19.20, LocalDateTime.now()));
+        assertEquals("Sensor ID must be between 3 and 10 characters", exMessage.getMessage());
+    }
+
+    @Test
+    void testSensorIdMaxFailure() {
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> new Sensor("1","sensor123456", "temperature", 19.20, LocalDateTime.now()));
+    }
+
+
 }
