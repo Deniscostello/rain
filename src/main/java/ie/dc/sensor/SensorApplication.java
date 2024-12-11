31b5d0da7f9ac03b77ff7a6869a3a0c9c9faaf61
Denis Costello <G00396309@atu.ie>
2024-12-11 08:37:24 -0500

    first commit

---
+++ src/main/java/ie/dc/sensor/SensorApplication.java
@@ -0,0 +1,13 @@
+package ie.dc.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/dc/sensor/SensorApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.dc.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
