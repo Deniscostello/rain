a7953dc3cf99a74ef1c6d25a17c0529b0badadb6
Denis Costello <G00396309@atu.ie>
2024-12-11 08:57:24 -0500

    Added Sensor class

---
+++ src/main/java/ie/dc/sensor/Sensor.java
@@ -0,0 +1,4 @@
+package ie.dc.sensor;
+
+public class Sensor {
+}
--- src/test/java/ie/dc/sensor/SensorSuccessTest.java
+++ src/test/java/ie/dc/sensor/SensorSuccessTest.java
@@ -1,4 +1,19 @@
 package ie.dc.sensor;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SensorSuccessTest {
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void testSensorIdSuccess(){
+        Sensor sensor = new Sensor();
+    }
+
+
+    @AfterEach
+    void tearDown() {}
 }
